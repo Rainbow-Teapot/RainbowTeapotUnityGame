e0df084291eb847b5a0cfd7a36e5d0e9c10f1f82
Merkado13 <marketyt@live.com>
2019-11-01 17:03:00 +0100

    Basic Delay Start Room

---
+++ RainbowTeapotGame/Assets/Scripts/PhotonScripts/NetworkController.cs
@@ -0,0 +1,19 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkController : MonoBehaviourPunCallbacks
+{ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Player has connected to the Photon master server in: " + PhotonNetwork.CloudRegion);
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+}
