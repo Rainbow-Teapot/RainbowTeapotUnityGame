82732e566f92e139712950774e225b2c52f1e75a
Merkado13 <marketyt@live.com>
2019-11-12 17:52:13 +0100

    Mug

---
+++ RainbowTeapotGame/Assets/Scripts/Mug.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mug : MonoBehaviour, IObstacle
+{
+    [SerializeField]
+    private float speedMultiplier = 0.45f;
+
+    public void ApplyEffect(CarMovement car)
+    {
+        Debug.Log("Reduzco la velocidad");
+        car.SetSpeedMultiplier(speedMultiplier);
+    }
+
+    public void DeApplyEffect(CarMovement car)
+    {
+        
+        car.SetSpeedMultiplier(1.0f);
+    }
+}
