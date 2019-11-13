b64d4db70897ead7b207e84dc0be2c7e3550679b
Merkado13 <marketyt@live.com>
2019-11-13 21:26:28 +0100

    Restoring

---
+++ RainbowTeapotGame/Assets/Scripts/AIMovement.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIMovement : MonoBehaviour, IMovement
+{
+    public float GetXOffset()
+    {
+        return 0.0f;
+    }
+}
