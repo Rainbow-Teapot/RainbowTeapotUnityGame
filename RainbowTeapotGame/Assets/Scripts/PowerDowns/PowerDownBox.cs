a5d3852cc811f9e7947ab5fa91053238c879b53e
Merkado13 <marketyt@live.com>
2019-11-24 19:11:02 +0100

    PowerDownArchitecture

---
+++ RainbowTeapotGame/Assets/Scripts/PowerDowns/PowerDownBox.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum powerDown {TEAPOT, DALSY, TURBO_BACK, PARACHUTE, LEGO};
+
+public class PowerDownBox : MonoBehaviour, IObstacle
+{
+   
+    public void ApplyEffect(CarMovement car)
+    {
+        PowerDownUser user = car.GetComponent<PowerDownUser>();
+        user.pickPowerDown();
+        Destroy(gameObject);
+    }
+
+    public void DeApplyEffect(CarMovement car)
+    {
+        throw new System.NotImplementedException();
+    }
+
+}
