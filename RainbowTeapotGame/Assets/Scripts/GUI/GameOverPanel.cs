9f24ded36bfa9c0f20ca8c28717490a2ba22ae41
Andrea Ro <andriusky0210@hotmail.com>
2019-12-02 12:50:15 +0100

    chsanrfds :airplane:

--- RainbowTeapotGame/Assets/Scripts/GUI/GameOverPanel.cs
+++ RainbowTeapotGame/Assets/Scripts/GUI/GameOverPanel.cs
@@ -2,9 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameOverPanel : MonoBehaviour
 {
+
+    public TextMeshProUGUI backToMenu;
+    public PlayerInfo playerInfo;
+    LanguageManager lang = new LanguageManager(); 
+
+    public void Start()
+    {
+        playerInfo = GameObject.Find("PlayerInfo").GetComponent<PlayerInfo>();
+        backToMenu.text = lang.getText(playerInfo.lang, 18);
+    }
+
     public void OnReturnToMenuButtonClicked()
     {
         SceneManager.LoadScene("Lobby");
--- RainbowTeapotGame/Assets/Scripts/LanguageManager.cs
+++ RainbowTeapotGame/Assets/Scripts/LanguageManager.cs
@@ -9,17 +9,18 @@ public class LanguageManager
     private language mLang;
 
     public enum word {Name, EnterName, Login, Training, Multiplayer, Credits, Configuration, Music, Sound, Language,
-        ChooseRider,Start, TryingToJoin,  LeaveGame, StartGame, Ready, GameStarts, Player
+        ChooseRider,Start, TryingToJoin,  LeaveGame, StartGame, Ready, GameStarts, Player, ReturnMenu
     }
     private word mWord;
 
 
-    string[,] names = new string[2, 18] {
+    string[,] names = new string[2, 19] {
 
         {"Name", "Enter player's name", "Login", "Training", "Multiplayer", "Credits", "Configuration", "Music" , "Sound", "Language",
-        "Choose your rider", "Start", "Trying to join room...",  "Leave game", "Start game", "Ready?","Game starts in", "Player"}, //Eng
+        "Choose your rider", "Start", "Trying to join room...",  "Leave game", "Start game", "Ready?","Game starts in", "Player", "Return to menu"}, //Eng
         {"Nombre", "Introduce el nombre", "Iniciar sesión", "Entrenamiento", "Multijugador", "CrÉditos", "Configuración", "Música", "Sonido",
-        "Idioma", "Elige a tu corredor", "Empezar", "Intentando entrar en la sala...",  "Abandonar partida", "Empezar partida", "¿Preparado?", "El juego comienza en" , "Jugador"} //Spa
+        "Idioma", "Elige a tu corredor", "Empezar", "Intentando entrar en la sala...",  "Abandonar partida", "Empezar partida", "¿Preparado?",
+            "El juego comienza en" , "Jugador", "Volver al menú"} //Spa
 
     };
 
